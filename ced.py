8c1ccd7cecf476f05ef98c58d1b0daddbb9d90e2
Kunal Sarkhel <kunal@zinc.(none)>
2009-03-27 19:23:06 -0400

    rewrote sections of code so each set of sourceview widgets are stored in self.documents

--- ced.py
+++ ced.py
@@ -2,16 +2,18 @@
 
 import pygtk
 pygtk.require('2.0')
-import gtk, gtksourceview
+import gtk, gtksourceview, mimetypes
 
 class Ced:
 	def __init__(self):
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		#self.sourcebuffers = []
+		self.documents = []
 		self.vbox = gtk.VBox(homogeneous = False, spacing = 0)
-		self.window.resize(400,400)
+		self.notebook = gtk.Notebook()
+		self.vbox.add(self.notebook)
+		self.window.resize(800,600)
 		self.window.add(self.vbox)
-		self.newEditor()
+		self.newPage("./ced.py")
 		self.window.show_all()
 
 	def delete_event(widget, event, data=None):
@@ -21,19 +23,34 @@ class Ced:
 	def destroy(widget, data=None):
 		print "destroy signal occurred"
 		gtk.main_quit()
-	
-	def newEditor(self):
-		self.sourcebuffer = gtksourceview.SourceBuffer()
-		self.langmanager = gtksourceview.SourceLanguagesManager()
-		self.codelang = self.langmanager.get_language_from_mime_type("text/x-python")
-		self.sourcebuffer.set_language(self.codelang)
-		self.sourcebuffer.set_highlight(True)
-		self.view = gtksourceview.SourceView(self.sourcebuffer)
-		self.view.set_show_line_numbers(True)
+
+	def newEditor(self, filename):
+		self.documents.append([gtksourceview.SourceBuffer(), gtksourceview.SourceLanguagesManager()])
+		a = len(self.documents)-1
+		self.documents[a].append(self.documents[a][1].get_language_from_mime_type(mimetypes.guess_type(filename)[0]))
+		self.documents[a][0].set_language(self.documents[a][2])
+		self.documents[a][0].set_highlight(True)
+		self.documents[a].append(gtksourceview.SourceView(self.documents[a][0]))
+		self.documents[a][3].set_show_line_numbers(True)
 		self.scrolledwindow = gtk.ScrolledWindow()
 		self.scrolledwindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-		self.scrolledwindow.add(self.view)
-		self.vbox.add(self.scrolledwindow)
+		self.scrolledwindow.add(self.documents[a][3])
+		self.notebook.append_page(self.scrolledwindow, None)
+
+	def newPage(self, filename):
+		b = len(self.documents)
+		a = [gtksourceview.SourceBuffer(), gtksourceview.SourceLanguagesManager()]
+		a.append(a[1].get_language_from_mime_type(mimetypes.guess_type(filename)[0]))
+		a[0].set_language(a[2])
+		a[0].set_highlight(True)
+		a.append(gtksourceview.SourceView(a[0]))
+		a[3].set_show_line_numbers(True)
+		a.append(gtk.ScrolledWindow())
+		a[4].set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		a[4].add(a[3])
+		self.documents.append(a)
+		self.notebook.append_page(self.documents[b][4], None)
+
 
 	def main(self):
 		self.window.connect("delete_event", self.delete_event)
